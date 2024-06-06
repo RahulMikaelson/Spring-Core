7e6a17e3d5d1c55ca1c6e1a9e6fe9022896f405c
RahulMikaelson <mikaelsonrahul@gmail.com>
2024-06-06 21:20:04 +0530

    Tight Couplining Code

---
+++ src/main/java/com/varun_matangi/spring/Game/MarioGame.java
@@ -0,0 +1,7 @@
+package com.varun_matangi.spring.Game;
+
+public class MarioGame {
+    public void consoleOutPut(){
+        System.out.println("Mario Game is Running");
+    }
+}
--- src/main/java/com/varun_matangi/spring/SpringCoreApplication.java
+++ src/main/java/com/varun_matangi/spring/SpringCoreApplication.java
@@ -1,5 +1,7 @@
 package com.varun_matangi.spring;
 
+import com.varun_matangi.spring.Game.GameRunner;
+import com.varun_matangi.spring.Game.MarioGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreApplication.class, args);
+		//SpringApplication.run(SpringCoreApplication.class, args);
+		MarioGame marioGame = new MarioGame();
+		GameRunner gameRunner =new GameRunner(marioGame);
+		gameRunner.run();
 	}
 
 }
